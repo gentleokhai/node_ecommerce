4a152fb680e6c065543c3799a9bcdcba8b186d8f
qgatssdev <chivoomodu@gmail.com>
2024-02-07 23:51:04 +0100

    feat: delete files

--- dto/Admin.dto.ts
+++
@@ -1,6 +0,0 @@
-export interface CreateAdminInput {
-  email: string;
-  phoneNumber: string;
-  password: string;
-  currency: string;
-}
--- index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-app.listen(8000, () => {
-  console.log('App is listening to the port 8000');
-});
