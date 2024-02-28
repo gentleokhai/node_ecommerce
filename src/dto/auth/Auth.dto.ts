197088ab4de60694e50cd94061489f1748df26d6
qgatssdev <chivoomodu@gmail.com>
2024-02-28 16:05:41 +0100

    ci: fix build

--- src/dto/auth/Auth.dto.ts
+++ src/dto/auth/Auth.dto.ts
@@ -4,23 +4,34 @@ import { SignupPayload, SignInPayload } from './types';
 export class SignupValidationSchema implements SignupPayload {
   @IsString()
   @IsNotEmpty()
-  email!: string;
+  email: string;
 
   @IsPhoneNumber()
   @IsNotEmpty()
-  phoneNumber!: string;
+  phoneNumber: string;
 
   @IsString()
   @IsNotEmpty()
-  password!: string;
+  password: string;
+
+  constructor(email: string, phoneNumber: string, password: string) {
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.password = password;
+  }
 }
 
 export class LoginValidationSchema implements SignInPayload {
   @IsString()
   @IsNotEmpty()
-  email!: string;
+  email: string;
 
   @IsString()
   @IsNotEmpty()
-  password!: string;
+  password: string;
+
+  constructor(email: string, password: string) {
+    this.email = email;
+    this.password = password;
+  }
 }
